506d46bc96b5b0a600a3b3544ae6b7ec498ac6b3
thientm27 <denkhotieu1@gmail.com>
2023-10-31 12:07:11 +0700

    Model with getter

--- Assets/Scripts/GameScene/GameModel.cs
+++ Assets/Scripts/GameScene/GameModel.cs
@@ -1,9 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GameScene
 {
-    public class GameModel: MonoBehaviour
+    public class GameModel : MonoBehaviour
     {
-        
+        [SerializeField] private List<ModelSelector> modelSelector;
+        [SerializeField] private List<ModelSelected> modelSelected;
+
+        public GameObject GetSelector(SelectType selectType)
+        {
+            var result = modelSelector.FirstOrDefault(o => o.SelectType == selectType);
+            return result?.Prefabs;
+        }
+        public GameObject GetSelected(SelectType selectType)
+        {
+            var result = modelSelected.FirstOrDefault(o => o.SelectType == selectType);
+            return result?.Prefabs;
+        }
+    }
+
+    [Serializable]
+    public class ModelSelector
+    {
+        [SerializeField] private SelectType selectType;
+        [SerializeField] private GameObject prefabs;
+
+        public SelectType SelectType => selectType;
+
+        public GameObject Prefabs => prefabs;
+    }
+
+    [Serializable]
+    public class ModelSelected
+    {
+        [SerializeField] private SelectType selectType;
+        [SerializeField] private GameObject prefabs;
+        public SelectType SelectType => selectType;
+        public GameObject Prefabs => prefabs;
     }
 }
