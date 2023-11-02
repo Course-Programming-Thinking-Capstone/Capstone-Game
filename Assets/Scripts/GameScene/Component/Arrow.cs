dc9d21abde7d890f5e6c3eb202d904fdd1a361b5
thientm27 <denkhotieu1@gmail.com>
2023-11-02 17:24:21 +0700

    Generated and assign buttons

--- Assets/Scripts/GameScene/Component/Arrow.cs
+++ Assets/Scripts/GameScene/Component/Arrow.cs
@@ -1,7 +1,13 @@
+using UnityEngine.UI;
+
 namespace GameScene.Component
 {
     public class Arrow : Selector
     {
- 
+        private void Awake()
+        {
+            var button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(OnClickButton);
+        }
     }
 }
--- Assets/Scripts/GameScene/GameController.cs
+++ Assets/Scripts/GameScene/GameController.cs
@@ -1,20 +1,35 @@
-using System;
 using System.Collections.Generic;
+using GameScene.Component;
+using Services;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameScene
 {
     public class GameController : MonoBehaviour
     {
+        [Header("Reference model")] 
         [SerializeField] private GameView view;
         [SerializeField] private GameModel model;
 
-        // TESTING
+        [Header("Testing only")] 
         [SerializeField] private List<SelectType> generateList;
-        
+
+        // SYSTEM
+        private List<Arrow> storeSelector = new();
+        private List<Arrow> storeSelected = new();
+
         private void Awake()
         {
-            
+            // Load services
+            if (GameObject.FindGameObjectWithTag(Constants.ServicesTag) != null)
+            {
+                var services = GameObject.FindGameObjectWithTag(Constants.ServicesTag).GetComponent<GameServices>();
+            }
+            else
+            {
+                //  SceneManager.LoadScene(Constants.EntryScene);
+            }
         }
 
         private void Start()
@@ -27,9 +42,22 @@ namespace GameScene
             // Generate objects selector
             foreach (var o in generateList)
             {
-                var obj =  Instantiate(model.GetSelector(o));
+                var obj = Instantiate(model.GetSelector(o));
                 view.SetParentSelector(obj.transform);
+                storeSelector.Add(obj.GetComponent<Arrow>());
             }
+
+            // Assign callback for selector
+            foreach (var arrow in storeSelector)
+            {
+                arrow.Init(OnClickedSelector);
+            }
+        }
+
+
+        private void OnClickedSelector(SelectType selectType)
+        {
+            Debug.Log("Clicked" + selectType);
         }
     }
 }
