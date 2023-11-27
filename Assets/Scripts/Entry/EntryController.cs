2eceb21fb743323c1443684b915722b7ebc8e2a4
thientm27 <denkhotieu1@gmail.com>
2023-11-27 14:41:57 +0700

    Change platform

--- Assets/Scripts/Entry/EntryController.cs
+++ Assets/Scripts/Entry/EntryController.cs
@@ -1,56 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 using Services;
 using Audio;
 
 namespace Entry
 {
-	public class EntryController : MonoBehaviour
-	{
-		private const string soundObjectName = "Sound";
-		private const string UnUsed = "unused";
-		[SerializeField] private EntryModel model;
+    public class EntryController : MonoBehaviour
+    {
+        private const string soundObjectName = "Sound";
+        private const string UnUsed = "unused";
+        [SerializeField] private EntryModel model;
 
-		[SerializeField] private List<Sound> sounds;
-		[SerializeField] private Music music;
-		[SerializeField] private GameObject musicObject;
+        [SerializeField] private List<Sound> sounds;
+        [SerializeField] private Music music;
+        [SerializeField] private GameObject musicObject;
 
-		[Space(8.0f)]
-		[SerializeField] private float loadingTime = 3f;
+        [Space(8.0f)]
+        [SerializeField] private float loadingTime = 3f;
 
-		private bool isReady = false;
-		private GameServices gameServices = null;
+        private bool isReady = false;
+        private GameServices gameServices = null;
 
-		void Awake()
-		{
-			if (GameObject.FindGameObjectWithTag(Constants.ServicesTag) == null)
-			{
-				GameObject gameServiceObject = new(nameof(GameServices))
-				{
-					tag = Constants.ServicesTag
-				};
-				gameServices = gameServiceObject.AddComponent<GameServices>();
+        void Awake()
+        {
+            if (GameObject.FindGameObjectWithTag(Constants.ServicesTag) == null)
+            {
+                GameObject gameServiceObject = new(nameof(GameServices))
+                {
+                    tag = Constants.ServicesTag
+                };
+                gameServices = gameServiceObject.AddComponent<GameServices>();
 
-				// Instantie Audio
-				DontDestroyOnLoad(musicObject);
+                // Instantie Audio
+                DontDestroyOnLoad(musicObject);
 
-				GameObject soundObject = new(soundObjectName);
-				DontDestroyOnLoad(soundObject);
-				// Add Services
-				gameServices.AddService(new AudioService(music, sounds, soundObject));
-				gameServices.AddService(new APIService());
-				gameServices.AddService(new DisplayService());
-				gameServices.AddService(new InputService());
-				gameServices.AddService(new PlayerService());
-				gameServices.AddService(new GameService(model.TOSURL, model.PrivacyURL, model.RateURL));
-	//			gameServices.AddService(new FirebaseService(OnFetchSuccess));
+                GameObject soundObject = new(soundObjectName);
+                DontDestroyOnLoad(soundObject);
+                // Add Services
+                gameServices.AddService(new AudioService(music, sounds, soundObject));
+                gameServices.AddService(new APIService());
+                gameServices.AddService(new DisplayService());
+                gameServices.AddService(new InputService());
+                gameServices.AddService(new PlayerService());
+                gameServices.AddService(new GameService(model.TOSURL, model.PrivacyURL, model.RateURL));
+                //			gameServices.AddService(new FirebaseService(OnFetchSuccess));
 #if UNITY_ANDROID
-				gameServices.AddService(new AdsService(model.BannerIdAndroid, model.IntersIdAndroid, model.RewardedIdAndroid, model.RewardedInterstitialIdAndroid, model.AOAIdAndroid));
-				gameServices.AddService(new AppsFlyerService(model.AppsFlyerDevKey, model.AppFlyerAppIdAndroid));
+                //		gameServices.AddService(new AdsService(model.BannerIdAndroid, model.IntersIdAndroid, model.RewardedIdAndroid, model.RewardedInterstitialIdAndroid, model.AOAIdAndroid));
+                //	gameServices.AddService(new AppsFlyerService(model.AppsFlyerDevKey, model.AppFlyerAppIdAndroid));
 #elif UNITY_IPHONE || UNITY_IOS
 				gameServices.AddService(new AdsService(model.BannerIdIOS,model.IntersIdIOS,model.RewardedIdIOS,model.RewardedInterstitialIdIOS,model.AOAIdIOS));
 				gameServices.AddService(new AppsFlyerService(model.AppsFlyerDevKey, model.AppsFlyerAppIdIos));
@@ -59,66 +57,72 @@ namespace Entry
 			//	gameServices.AddService(new AppsFlyerService(model.AppsFlyerDevKey, UnUsed));
 #endif
 
-				// Get services
-				var displayService = gameServices.GetService<DisplayService>();
-				var audioService = gameServices.GetService<AudioService>();
-				var playerService = gameServices.GetService<PlayerService>();
-			
-				// --------------------------- Ads ---------------------------------
-				// Setting ads from firebase
-				//firebaseServices.OnLimitTimeAdsChanged = adsServices.SetLimitTimeShowAds;
-				//firebaseServices.OnShowAppOpenAdChange = adsServices.OnShowAppOpenAdChange;
-				// ------------------------------------------------------------------
+                // Get services
+                var displayService = gameServices.GetService<DisplayService>();
+                var audioService = gameServices.GetService<AudioService>();
+                var playerService = gameServices.GetService<PlayerService>();
 
-				// --------------------------- Audio ---------------------------------
-				// Set Volume
-				playerService.OnMusicVolumeChange = audioService.SetMusicVolume;
-				playerService.OnSoundVolumeChange = audioService.SetSoundVolume;
+                // --------------------------- Ads ---------------------------------
+                // Setting ads from firebase
+                //firebaseServices.OnLimitTimeAdsChanged = adsServices.SetLimitTimeShowAds;
+                //firebaseServices.OnShowAppOpenAdChange = adsServices.OnShowAppOpenAdChange;
+                // ------------------------------------------------------------------
 
-				playerService.OnVibrateChange = audioService.SetVibrate;
+                // --------------------------- Audio ---------------------------------
+                // Set Volume
+                playerService.OnMusicVolumeChange = audioService.SetMusicVolume;
+                playerService.OnSoundVolumeChange = audioService.SetSoundVolume;
 
-				audioService.MusicVolume = playerService.GetMusicVolume();
-				audioService.SoundVolume = playerService.GetSoundVolume();
+                playerService.OnVibrateChange = audioService.SetVibrate;
 
-				audioService.VibrateOn = playerService.GetVibrate();
+                audioService.MusicVolume = playerService.GetMusicVolume();
+                audioService.SoundVolume = playerService.GetSoundVolume();
 
-				audioService.MusicOn = true;
-				audioService.SoundOn = true;
+                audioService.VibrateOn = playerService.GetVibrate();
 
-				audioService.StopMusic();
-				playerService.LoadData();
-				// ------------------------------------------------------------------
-			}
-		}
-		private void Start()
-		{
-			Loading();
-		}
-		private void Loading()
-		{
-			StartCoroutine(Wait());
-		}
-		private IEnumerator Wait()
-		{
-			float timer = loadingTime * 0.1f;
-			while (timer < loadingTime * 1.1f)
-			{
-				timer += Time.deltaTime;
-				yield return null;
-			}
-			timer = 0;
-			while (!isReady && timer <= loadingTime)
-			{
-				yield return null;
-				timer += Time.deltaTime;
-			}
-			SceneManager.LoadScene(Constants.MainMenu);
-		}
-		private void OnFetchSuccess()
-		{
-			Time.timeScale = 1.0f;
-			//gameServices.GetService<AdsService>().LoadAppOpenAd();
-			isReady = true;
-		}
-	}
+                audioService.MusicOn = true;
+                audioService.SoundOn = true;
+
+                audioService.StopMusic();
+                playerService.LoadData();
+                // ------------------------------------------------------------------
+            }
+        }
+
+        private void Start()
+        {
+            Loading();
+        }
+
+        private void Loading()
+        {
+            StartCoroutine(Wait());
+        }
+
+        private IEnumerator Wait()
+        {
+            float timer = loadingTime * 0.1f;
+            while (timer < loadingTime * 1.1f)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            timer = 0;
+            while (!isReady && timer <= loadingTime)
+            {
+                yield return null;
+                timer += Time.deltaTime;
+            }
+
+            SceneManager.LoadScene(Constants.MainMenu);
+        }
+
+        private void OnFetchSuccess()
+        {
+            Time.timeScale = 1.0f;
+            //gameServices.GetService<AdsService>().LoadAppOpenAd();
+            isReady = true;
+        }
+    }
 }
