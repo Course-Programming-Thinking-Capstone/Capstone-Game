03b6c675c1825b5221f6a3982fe8fe2d8c39747e
thientm27 <denkhotieu1@gmail.com>
2023-11-27 17:39:04 +0700

    Main view method

--- Assets/Scripts/MainScene/MainSceneView.cs
+++ Assets/Scripts/MainScene/MainSceneView.cs
@@ -1,19 +1,43 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainScene
 {
     public class MainSceneView : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("MainMenu")]
+        [SerializeField] private TextMeshProUGUI userCoin;
+        [SerializeField] private TextMeshProUGUI userDiamond;
+        [SerializeField] private TextMeshProUGUI userName;
+        [SerializeField] private TextMeshProUGUI userLevel;
+        [SerializeField] private TextMeshProUGUI userProcess;
+        [SerializeField] private Slider userProcessSlider;
+
+        public void SetDisplayUserCoin(int value)
         {
-        
+            userCoin.text = value.ToString();
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SetDisplayUserDiamond(int value)
         {
-        
+            userDiamond.text = value.ToString();
+        }
+
+        public void SetDisplayUserName(string value)
+        {
+            userName.text = value;
+        }
+
+        public void SetDisplayUserLevel(int value)
+        {
+            userLevel.text = value.ToString();
+        }
+
+        public void SetDisplayUserProcess(string value, float fillValue)
+        {
+            userProcess.text = value;
+            userProcessSlider.value = fillValue > 1 ? 1 : fillValue;
         }
     }
 }
