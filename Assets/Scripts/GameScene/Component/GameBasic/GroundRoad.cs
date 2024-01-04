7530ec715a585923fb5ee9eaf890cbc26540ae5f
thientm27 <denkhotieu1@gmail.com>
2024-01-04 20:52:31 +0700

    clean up

--- Assets/Scripts/GameScene/Component/GameBasic/GroundRoad.cs
+++ Assets/Scripts/GameScene/Component/GameBasic/GroundRoad.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GameScene.Component.GameBasic
 {
     public class GroundRoad : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer rendererGround;
+        [SerializeField] private SelectType selectType = SelectType.None;
+         private UnityAction<SelectType> callBack;
 
-        public void ChangeRender(Sprite newSprite)
+        public void ChangeRender(Sprite newSprite, SelectType type)
         {
             rendererGround.sprite = newSprite;
+            selectType = type;
+        }
+
+        public void OnClick()
+        {
+            callBack.Invoke(selectType);
         }
     }
 }
