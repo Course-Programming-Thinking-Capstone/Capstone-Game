c3c96ae2d60793a7f22cd5d7eb42e21bff6e720d
thientm27 <denkhotieu1@gmail.com>
2024-03-03 15:07:30 +0700

    popup animation

--- Assets/Scripts/GenericPopup/PopupAdditive.cs
+++ Assets/Scripts/GenericPopup/PopupAdditive.cs
@@ -1,15 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using Utilities;
 
 namespace GenericPopup
 {
     public class PopupAdditive : MonoBehaviour
     {
+        [SerializeField]
+        private Animator animator;
+        private readonly int exit = Animator.StringToHash("Exit");
 
-        public void ClosePopup()
+        protected void ClosePopup()
         {
+            if (animator != null)
+            {
+                animator.SetBool(exit, true);
+                StartCoroutine(CloseDelay(0.5f));
+            }
+
             PopupHelpers.Close(gameObject.scene);
         }
-        
+
+        private IEnumerator CloseDelay(float delayTime)
+        {
+            yield return new WaitForSeconds(delayTime);
+
+            PopupHelpers.Close(gameObject.scene);
+        }
     }
 }
