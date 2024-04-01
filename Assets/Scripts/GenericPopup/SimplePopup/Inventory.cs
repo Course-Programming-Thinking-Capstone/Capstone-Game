543645188aff853d1edc4356b985c716c2cd23df
thientm27 <denkhotieu1@gmail.com>
2024-04-01 15:49:25 +0700

    Design inventory

--- Assets/Scripts/GenericPopup/SimplePopup/Inventory.cs
+++ Assets/Scripts/GenericPopup/SimplePopup/Inventory.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+namespace GenericPopup.SimplePopup
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Inventory : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
