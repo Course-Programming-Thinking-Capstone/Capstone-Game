2b436609bbd6fe5d8ef967b23c648affed59a221
thientm27 <denkhotieu1@gmail.com>
2024-04-01 17:24:03 +0700

    Inventory scripts setuop, add plugin Serialize Dictionary

--- Assets/Scripts/GenericPopup/Inventory/InventItem.cs
+++ Assets/Scripts/GenericPopup/Inventory/InventItem.cs
@@ -1,16 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace GenericPopup.Inventory
 {
     public class InventItem : MonoBehaviour
     {
-        public UnityAction onClick;
+        [SerializeField]
+        private List<Sprite> rateRender;
+        [SerializeField] private Image renderImg;
+        [SerializeField] private Image rateRenderImg;
+        [SerializeField] private TextMeshProUGUI quantityTxt;
+        private UnityAction onClick;
 
+        public void InitializedItem(Sprite sprite, int quantity, UnityAction callBack)
+        {
+            renderImg.sprite = sprite;
+            quantityTxt.text = quantity.ToString();
+            onClick = callBack;
+        }
 
         public void OnClickThisItem()
         {
             onClick?.Invoke();
         }
+    }
+
+    public enum ValueRate
+    {
+        Orange,
+        Red,
+        Purple,
+        Blue,
+        Green,
+        Gray
     }
 }
